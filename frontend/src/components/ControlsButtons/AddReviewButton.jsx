9d5dc9b0ee46e57701732350ac0939e81eaa7e32
maraned05 <nedelcu.mara.02@gmail.com>
2025-03-31 17:50:38 +0300

    frontend done

--- frontend/src/components/ControlsButtons/AddReviewButton.jsx
+++ frontend/src/components/ControlsButtons/AddReviewButton.jsx
@@ -4,7 +4,7 @@ import './AddReviewButton.css'
 
 function AddReviewButton ({ setIsOpen }) {
     return (
-        <button className="addReviewButton" onClick={() => setIsOpen(true)}>
+        <button role="addReviewButton" className="addReviewButton" onClick={() => setIsOpen(true)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
             </svg>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
       '@': '/src',
     },
   },
+  test: {
+    environment: 'jsdom', 
+  },
 });
