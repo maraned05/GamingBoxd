98342cd1381b17d21080afdba0f55217ecd5f216
maraned05 <nedelcu.mara.02@gmail.com>
2025-05-24 12:29:01 +0300

    deploy commit 2

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,6 @@ const server = require('./server');
 const { BACKEND_PORT } = require('./utils/config');
 const { BACKEND_URL } = require('./utils/config');
 
-server.listen(BACKEND_PORT, () => {
+server.listen(BACKEND_PORT, '0.0.0.0', () => {
     console.log(`Server running on ${BACKEND_URL}`)
 });
