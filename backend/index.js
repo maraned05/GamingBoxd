d0a64985cd4dd6ffab431563686666fe91b1ba43
maraned05 <nedelcu.mara.02@gmail.com>
2025-05-24 12:20:24 +0300

    deployment commit

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,5 @@ const { BACKEND_PORT } = require('./utils/config');
 const { BACKEND_URL } = require('./utils/config');
 
 server.listen(BACKEND_PORT, () => {
-    console.log(`Server running on ${BACKEND_URL}:${BACKEND_PORT}`)
+    console.log(`Server running on ${BACKEND_URL}`)
 });
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL = "http://localhost:5000";
+// export const BACKEND_URL = "http://localhost:5000";
 // export const FRONTEND_URL = "http://localhost:3000";
-//export const BACKEND_URL = "http://192.168.100.34:5000";
-export const FRONTEND_URL = "http://localhost:3000";
+
+export const FRONTEND_URL = "http://16.171.26.2";
+export const BACKEND_URL = "http://16.171.26.2:3001";
