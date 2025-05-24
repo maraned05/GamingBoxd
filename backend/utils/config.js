e51228f114fd6af6fa2cce11c3c091d9b0bbdf5d
maraned05 <nedelcu.mara.02@gmail.com>
2025-05-24 18:52:11 +0300

    random

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,7 @@
 // const BACKEND_URL = "http://localhost";
 // const FRONTEND_URL = "http://localhost:3000";
-const BACKEND_URL = "http://16.171.26.2:3001";
-const FRONTEND_URL = "http://16.171.26.2";
+const BACKEND_URL = "http://13.60.157.191:3001";
+const FRONTEND_URL = "http://13.60.157.191";
 
 const BACKEND_PORT = 3001;
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // export const BACKEND_URL = "http://localhost:5000";
 // export const FRONTEND_URL = "http://localhost:3000";
 
-export const FRONTEND_URL = "http://16.171.26.2";
-export const BACKEND_URL = "http://16.171.26.2:3001";
+export const FRONTEND_URL = "http://13.60.157.191";
+export const BACKEND_URL = "http://13.60.157.191:3001";
