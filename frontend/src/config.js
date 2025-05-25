92bfe6db6357c7474531589cb23326a512064e49
maraned05 <nedelcu.mara.02@gmail.com>
2025-05-25 13:28:37 +0300

    https3

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,5 +2,5 @@
 // export const FRONTEND_URL = "http://localhost:3000";
 
 export const FRONTEND_URL = "http://51.21.180.192";
-export const BACKEND_URL = "http://51.21.180.192:3001";
-//export const BACKEND_URL = "https://7d4e-51-21-180-192.ngrok-free.app";
+//export const BACKEND_URL = "http://51.21.180.192:3001";
+export const BACKEND_URL = "https://7d4e-51-21-180-192.ngrok-free.app";
