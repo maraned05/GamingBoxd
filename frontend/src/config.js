d31ad533149fa089116e4f7cf9a1215949b19ac3
maraned05 <nedelcu.mara.02@gmail.com>
2025-05-25 13:19:08 +0300

    first commit for https

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 // export const BACKEND_URL = "http://localhost:5000";
 // export const FRONTEND_URL = "http://localhost:3000";
 
-export const FRONTEND_URL = "http://13.60.157.191";
-export const BACKEND_URL = "http://13.60.157.191:3001";
+export const FRONTEND_URL = "http://51.21.180.192";
+// export const BACKEND_URL = "http://51.21.180.192:3001";
+export const BACKEND_URL = "https://7d4e-51-21-180-192.ngrok-free.app";
