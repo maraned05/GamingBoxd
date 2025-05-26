7c2353fcd18f9c7b85ba42097dc0ec3c1ecf88e6
maraned05 <nedelcu.mara.02@gmail.com>
2025-05-26 23:04:38 +0300

    3

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,7 @@
 // const BACKEND_URL = "http://localhost";
 // const FRONTEND_URL = "http://localhost:3000";
-const BACKEND_URL = "http://51.21.180.192:3001";
-const FRONTEND_URL = "http://51.21.180.192";
+const BACKEND_URL = "https://13.60.190.167:3001";
+const FRONTEND_URL = "http://13.60.190.167";
 
 const BACKEND_PORT = 3001;
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // export const BACKEND_URL = "http://localhost:5000";
 // export const FRONTEND_URL = "http://localhost:3000";
 
-export const FRONTEND_URL = "http://51.21.180.192";
-//export const BACKEND_URL = "http://51.21.180.192:3001";
-export const BACKEND_URL = "https://7d4e-51-21-180-192.ngrok-free.app";
+export const FRONTEND_URL = "http://13.60.190.167";
+export const BACKEND_URL = "https://13.60.190.167:3001";
+//export const BACKEND_URL = "https://7d4e-51-21-180-192.ngrok-free.app";
