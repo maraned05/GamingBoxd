c23c02e7c37a1abd190e6e95c17a6a8321d8e883
maraned05 <nedelcu.mara.02@gmail.com>
2025-05-27 12:38:26 +0300

    9

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,6 @@
 // const BACKEND_URL = "http://localhost";
 // const FRONTEND_URL = "http://localhost:3000";
-const BACKEND_URL = "https://51.20.70.237:3001";
+const BACKEND_URL = "http://51.20.70.237:3001";
 const FRONTEND_URL = "http://51.20.70.237";
 
 const BACKEND_PORT = 3001;
