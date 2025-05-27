bf7e373b83470328013ebae08170392967664f27
maraned05 <nedelcu.mara.02@gmail.com>
2025-05-27 10:19:38 +0300

    7

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,5 +2,5 @@
 // export const FRONTEND_URL = "http://localhost:3000";
 
 export const FRONTEND_URL = "http://13.53.93.235";
-export const BACKEND_URL = "https://13.53.93.235:3001";
+export const BACKEND_URL = "http://13.53.93.235:3001";
 //export const BACKEND_URL = "https://dce8-56-228-12-73.ngrok-free.app";
