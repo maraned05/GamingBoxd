f53554d6aae0521ea892b58dae7d6662458ce8c6
maraned05 <nedelcu.mara.02@gmail.com>
2025-06-03 11:04:54 +0300

    2af email

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    }
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5a18a4e54c82bc",
+    pass: "****a8d0"
+  }
 });
 
 const sendVerificationEmail = async (email, code) => {
